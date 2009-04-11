38b1e6c64d999c8d7b199b07015a087e237f632e
Omar Pera <campbell.sx@gmail.com>
2009-04-11 17:24:21 +0000

    añadido módulo UserTask de uno de google para hacer tareas en otro thread, de momento añadido en la main activity. Mira haber que te parece, no acepta tirar excepciones en el doBackground, o algo raro...mira si se ocurre algo mejor para el tema de errores.
    tambien una mariconada en caso de error con un dialog de 'salir o reintentar'.
    
    creo que no queda del todo mal :D

--- client/Lottodroid/src/com/lottodroid/Lottodroid.java
+++ client/Lottodroid/src/com/lottodroid/Lottodroid.java
@@ -1,9 +1,10 @@
 package com.lottodroid;
 
-import java.util.List;
-
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,20 +14,23 @@ import com.lottodroid.communication.LotteryFetcher;
 import com.lottodroid.communication.LotteryInfoUnavailableException;
 import com.lottodroid.communication.MockLotteryFetcher;
 import com.lottodroid.communication.ServerLotteryFetcher;
-import com.lottodroid.model.Lottery;
+import com.lottodroid.util.UserTask;
 
 /**
- * Activity for the main screen. 
+ * Activity for the main screen.
  */
 public class Lottodroid extends ListActivity {
-  
+
+  public static final String TAG = Lottodroid.class.toString();
+
   /**
    * If the offline mode is true, no communication with the server will be performed: mock data will
    * be generated instead. See the implementations for {@link LotteryFetcher}.
+   * 
    * TODO: Move this flag to the command-line flag list
    */
   private static boolean OFFLINE_MODE = true;
-  
+
   /**
    * Called when the activity is first created.
    */
@@ -34,29 +38,89 @@ public class Lottodroid extends ListActivity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
+    fetchDataForMainView();
+  }
 
+  /** 
+   * Fetches and set the data that the main view will display 
+   */
+  private void fetchDataForMainView() {
     try {
-      setListAdapter(new MainViewAdapter(this, fetchDataForMainView()));
-    } catch (LotteryInfoUnavailableException e) {
-      // TODO: como queremos tratar esto?
-      Log.e("Lottodroid", "Ouch, no data!", e);
-    }   
+      new FetchAllLotteryResultsTask().execute();
+    } catch (IllegalStateException e) {
+      Log.e(TAG, "Fatal error: " + e.getMessage());
+    }
   }
 
-  /** Fetches the data that the main view will display: the last results for every lottery type */
-  private List<Lottery> fetchDataForMainView() throws LotteryInfoUnavailableException {
-    LotteryFetcher dataFetcher = OFFLINE_MODE ? new MockLotteryFetcher() : new ServerLotteryFetcher();
-    return dataFetcher.retrieveLastAllLotteries();
-    
-    // El details activity hará este tipo de llamadas
-    //return ServerController.retrieveLastBonolotos(0, 5);
-  }
-  
+  /**
+   * This method will be called when an item in the list is selected, starting a new activity with
+   * all the details of the lottery type selected.
+   */
   @Override
   protected void onListItemClick(ListView l, View v, int position, long id) {
     super.onListItemClick(l, v, position, id);
 
     Intent i = new Intent(this, DetailsActivity.class);
-    startActivity(i); 
-  }  
+    startActivity(i);
+  }
+
+  /**
+   * Task that fetches the data that the main view will display: the last results for every lottery
+   * type.
+   */
+  private class FetchAllLotteryResultsTask extends UserTask<Void, Void, MainViewAdapter> {
+
+    public MainViewAdapter doInBackground(Void... params) {
+      LotteryFetcher dataFetcher = OFFLINE_MODE ? 
+                                              new MockLotteryFetcher()
+                                            : new ServerLotteryFetcher();
+      try {
+        // Just to see the behavior
+        Thread.sleep(2000);
+
+        return new MainViewAdapter(Lottodroid.this, dataFetcher.retrieveLastAllLotteries());
+        
+      } catch (LotteryInfoUnavailableException e) {
+        Log.e(TAG, "Lottery info unavailable", e);
+      } catch (InterruptedException e) {
+        ;
+      } 
+      
+      return null;
+    }
+
+    @Override
+    public void end(MainViewAdapter adapter) {
+      // Adapter set to null if there is an error or an exception thrown
+      adapter = null;
+      if (adapter == null) {
+        OnClickListener retryListener = new OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            dialog.cancel();
+            fetchDataForMainView();
+          }
+        };
+
+        OnClickListener exitListener = new OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            Lottodroid.this.finish();
+          }
+        };
+
+        new AlertDialog.Builder(Lottodroid.this)
+          .setTitle("Error")
+          .setMessage("No se han podido encontrar los resultados de los últimos sorteos")
+          .setCancelable(false)
+          .setPositiveButton("Reintentar", retryListener)
+          .setNegativeButton("Salir", exitListener)
+          .show();
+        
+      } else {
+        setListAdapter(adapter);
+      }
+    }
+
+  }
 }
