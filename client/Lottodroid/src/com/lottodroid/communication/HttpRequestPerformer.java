38f8b9e2d4b32ee5292da8988493d525b00f576b
Omar Pera <campbell.sx@gmail.com>
2009-04-16 22:12:32 +0000

    minor improvement in httprequestperformer, timeout added

--- client/Lottodroid/src/com/lottodroid/communication/HttpRequestPerformer.java
+++ client/Lottodroid/src/com/lottodroid/communication/HttpRequestPerformer.java
@@ -1,9 +1,11 @@
 package com.lottodroid.communication;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
@@ -12,29 +14,34 @@ import java.net.URL;
  */
 class HttpRequestPerformer {
 
+  /** Time to wait to the server until the connection is canceled, it is specified in milliseconds */
+  private static int CONNECT_TIMEOUT = 5000;
+  private static int READ_TIMEOUT = 3000;
+  
   /**
    * Forms an HTTP request
    * 
    * @param url The URL to request for.
    * @return The response as a String
-   * @throws Exception
+   * @throws IOException 
+   * @throws MalformedURLException 
    */
-  public static String getResponse(String url) throws Exception {
-
-    HttpURLConnection http = (HttpURLConnection) new URL(url.toString()).openConnection();
+  public static String getResponse(String url) throws MalformedURLException, IOException {
+    HttpURLConnection http = (HttpURLConnection) new URL(url).openConnection();
+    http.setRequestMethod("GET");
     http.setDoInput(true);
-    http.setDoOutput(true);
+    http.setConnectTimeout(CONNECT_TIMEOUT);
+    http.setReadTimeout(READ_TIMEOUT);
 
     try {
-      InputStream is = http.getInputStream();
-
-      return toString(is);
-
+      return toString(http.getInputStream());
     } finally {
       http.getInputStream().close();
 
       if (http.getErrorStream() != null)
         http.getErrorStream().close();
+
+      http.disconnect();
     }
 
   }
@@ -44,11 +51,11 @@ class HttpRequestPerformer {
    * 
    * @param inputStream The InputStream to read from.
    * @return The contents of the InputStream as a String.
-   * @throws Exception
+   * @throws IOException 
    */
-  private static String toString(InputStream inputStream) throws Exception {
+  private static String toString(InputStream inputStream) throws IOException {
     StringBuilder outputBuilder = new StringBuilder();
-    String string;
+    String string = null;
     
     if (inputStream != null) {
       BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
