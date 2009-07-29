765cd304ba3a1f3232cf939f98520c9ccedb1027
Omar Pera <campbell.sx@gmail.com>
2009-07-29 17:22:29 +0000

    Cambiamos descripcion y arreglamos links del About

--- server/modules/frontend/views/contact.php
+++ server/modules/frontend/views/contact.php
@@ -8,9 +8,9 @@
 	<div class="wrap" style="width: 80%;">
 		<!-- <span class="stepApp">Apps: </span> -->
 
-		<span class="step"><a href="#anyrss">AnyRSS reader</a><span class="new-text">New!</span></span>
-		<span class="step"><a href="#fmylife">F*ck my life</a><span class="new-text">New!</span></span>
-		<span class="step"><a href="#dailystuff">Daily stuff</a></span>
+		<span class="step"><a href="index.php#anyrss">AnyRSS reader</a><span class="new-text">New!</span></span>
+		<span class="step"><a href="index.php#fmylife">F*ck my life</a><span class="new-text">New!</span></span>
+		<span class="step"><a href="index.php#dailystuff">Daily stuff</a></span>
 		
 
 	</div>
@@ -21,10 +21,16 @@
 		<div class="col">
 			<h3>About</h3>
 			<p>
-				Meanwhile we think a good <em>About</em> you can follow us on twitter, 
-				<a href="http://twitter.com/espinchi">@espinchi</a> and <a href="http://twitter.com/ompemi">@ompemi</a>
-				
-				
+				We are <a href="http://www.linkedin.com/in/omarpera">Omar Pera Mira</a> 
+				and <a href="http://www.linkedin.com/in/pablopera">Pablo Pera Mira</a>, developers starting
+				out in the Android world. 
+			</p>
+			<p>
+				You can follow us on twitter, 
+				<a href="http://twitter.com/espinchi">@espinchi</a> and <a href="http://twitter.com/ompemi">@ompemi</a>.
+			</p>
+			<p>	
+				Send us an email to <img alt="androidsx at the google mail server" src="web/img/email.png" />	
 			</p>
 		</div>
 	</div>
